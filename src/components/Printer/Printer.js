5c32a8226edc52836ea00cbad677f91b0dab8108
Rebecca Axelsson <ra223ai@student.lnu.se>
2021-10-20 18:19:01 +0200

    💄  Add styling

--- src/components/Printer/Printer.js
+++ src/components/Printer/Printer.js
@@ -1,13 +1,16 @@
 import './Printer.css'
 
-const Printer = ({ text }) => {
-  return ( 
-    <div className="textField">
-      {text.map((t, index) => (
-        <p className={t.type} key={index}>
-          <span className="indexContainer">[{index}]</span> {t.string}&nbsp;
-        </p>
-      ))}
+const Printer = ({ text, resetText }) => {
+  return (
+    <div className="printerContainer">
+      <div className="textField">
+        {text.map((t, index) => (
+          <p className={t.type} key={index}>
+            <span className="indexContainer">[{index}]</span> {t.string}&nbsp;
+          </p>
+        ))}
+      </div>
+      <button className="clearBtn" onClick={resetText}>Clear document</button>
     </div>
    );
 }
