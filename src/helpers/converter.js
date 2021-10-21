e336c4f43d454709427d6b2a29928748c5765b37
Rebecca Axelsson <ra223ai@student.lnu.se>
2021-10-21 08:38:15 +0200

    ♻️  Refactor to class

--- src/helpers/converter.js
+++ src/helpers/converter.js
@@ -2,33 +2,40 @@ import { RegularSentence } from '@rebaxe/parser/src/RegularSentence'
 import { Expression } from '@rebaxe/parser/src/Expression'
 import { Question } from '@rebaxe/parser/src/Question'
 
-export function convertSentence (sentence) {
-  if (isRegular(sentence)) {
-    return {
-      string: sentence.getStringSentence,
-      type: 'regular'
-    }
-  } else if (isExpression(sentence)) {
-    return {
-      string: sentence.getStringSentence,
-      type: 'expression'
-    }
-  } else if (isQuestion(sentence)) {
-    return {
-      string: sentence.getStringSentence,
-      type: 'question'
+/**
+ * Represents a converter.
+ *
+ * @class
+ */
+export class Converter {
+  convertSentence (sentence) {
+    if (this._isRegular(sentence)) {
+      return {
+        string: sentence.getStringSentence,
+        type: 'regular'
+      }
+    } else if (this._isExpression(sentence)) {
+      return {
+        string: sentence.getStringSentence,
+        type: 'expression'
+      }
+    } else if (this._isQuestion(sentence)) {
+      return {
+        string: sentence.getStringSentence,
+        type: 'question'
+      }
     }
   }
-}
-
-function isRegular(sentence) {
-  return sentence instanceof RegularSentence
-}
-
-function isExpression(sentence) {
-  return sentence instanceof Expression
-}
-
-function isQuestion(sentence) {
-  return sentence instanceof Question
+  
+  _isRegular(sentence) {
+    return sentence instanceof RegularSentence
+  }
+  
+  _isExpression(sentence) {
+    return sentence instanceof Expression
+  }
+  
+  _isQuestion(sentence) {
+    return sentence instanceof Question
+  }
 }
